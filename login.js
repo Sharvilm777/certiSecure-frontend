566e4cf0c787927b2b97fc4309b4e209519327d4
Sharvil <sharvilm777@gmail.com>
2023-05-15 14:48:05 +0530

    Role defined in frontend

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginBtn = document.getElementById("loginBtn");
-
+import jwtDecode from "jwt-decode";
 loginBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   const data = {
@@ -22,7 +22,10 @@ loginBtn.addEventListener("click", async (e) => {
     const result = await response.json();
     console.log(result);
     let authToken = result.authToken;
+    const { role } = jwtDecode(authToken);
+    console.log(role);
     localStorage.setItem("authToken", authToken);
+    localStorage.setItem("role", role);
     alert("User Logged IN Succesfully");
     setTimeout(() => {
       baseUrl = "https://transcendent-snickerdoodle-40ccb7.netlify.app";
